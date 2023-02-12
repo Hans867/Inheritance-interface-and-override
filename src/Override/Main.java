bb0084e3987f9a4a3d8e45c2daa50c58b02c760b
Hans867 <hans867m@stud.kea.dk>
2023-02-12 21:10:41 +0100

    CS101-A2

---
+++ src/Override/Main.java
@@ -0,0 +1,12 @@
+package Override;
+
+public class Main {
+    public static void main(String[] args) {
+        OldSchoolRap bigge = new OldSchoolRap();
+        NewSchoolRap ledZeppelin = new NewSchoolRap();
+
+        bigge.play();
+        ledZeppelin.play();
+
+    }
+}
---
+++ src/Override/OldSchoolRap.java
@@ -0,0 +1,10 @@
+package Override;
+
+public class OldSchoolRap extends RapSong {
+
+    @Override
+    public void play() {
+        System.out.println("Playing oldschool rap");
+    }
+
+}
